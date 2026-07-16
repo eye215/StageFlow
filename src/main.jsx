aa6fad7bffc91561de45de35e3e0fc73dff7be96
yuriKim <yrkim@naddle.net>
2026-07-16 21:17:48 +0900

    Refine scene card hierarchy

--- src/main.jsx
+++ src/main.jsx
@@ -22,6 +22,7 @@ import './actor-run-flow.css'
 import './connected-overview.css'
 import './actor-home.css'
 import './scene-hub.css'
+import './scene-card-polish.css'
 
 class AppErrorBoundary extends React.Component {
   constructor(props) {
